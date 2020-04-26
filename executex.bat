c5ac75a02e369aa14aa01c08bafeb81e7c5b13a5
PharmTeX code development site <contact@pharmtex.org>
2020-04-26 11:20:24 +0200

    Update for v. 1.13

--- executex.bat
+++ executex.bat
@@ -1,6 +1,6 @@
 @echo off
 REM PharmTeX Windows shell script, part of the PharmTeX platform.
-REM Copyright (C) 2018 Christian Hove Rasmussen (contact@pharmtex.org).
+REM Copyright (C) 2020 Christian Hove Claussen (contact@pharmtex.org).
 REM This program is free software: You can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program (see file named LICENSE). If not, see <https://www.gnu.org/licenses/>.
 
 REM Change this path to fit your installation
