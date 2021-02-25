659a6d4989e222e2068c4d2e83d91f010022417d
PharmTeX code development site <contact@pharmtex.org>
2021-02-25 12:43:04 +0100

    Update for v. 2.2.

--- runlatex.sh
+++ runlatex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # PharmTeX Linux start script, part of the PharmTeX platform.
-# Copyright (C) 2020 Christian Hove Claussen (contact@pharmtex.org).
+# Copyright (C) 2021 Christian Hove Claussen (contact@pharmtex.org).
 # This program is free software: You can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program (see file named LICENSE). If not, see <https://www.gnu.org/licenses/>.
 
 # Path to texfs
